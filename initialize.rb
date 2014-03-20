2cab146d855b8cb603d17cb8ae0ec45e6debe847
Lorin Thwaits <lorin@Lorins-MacBook-Air.local>
2014-03-20 10:27:04 -0700

    Forgot the initialize :)

--- initialize.rb
+++ initialize.rb
@@ -2,23 +2,23 @@ line = [] #string[]
 ret = ""
 
 File.open("start.txt", "r") do |infile|
-line = infile.gets.split(' ')
-@numDiscs = line[0].to_i
-numPegs = line[1].to_i
+	line = infile.gets.split(' ')
+	@numDiscs = line[0].to_i
+	numPegs = line[1].to_i
 
-	start = Move.new(nil, numPegs)
+	@start = Move.new(nil, numPegs)
 	@finish = Move.new(nil, numPegs)
 
-	# Where the discs all start
-line = infile.gets.split(' ')
+		# Where the discs all start
+	line = infile.gets.split(' ')
 	thisDisc = 1
 	line.each do |pegNum|
-		start.pegs[pegNum.to_i-1].unshift thisDisc
+		@start.pegs[pegNum.to_i-1].unshift thisDisc
 		thisDisc += 1
 	end
-	
-	# And where they should all end up
-line = infile.gets.split(' ')
+		
+		# And where they should all end up
+	line = infile.gets.split(' ')
 	thisDisc = 1
 	line.each do |pegNum|
 		@finish.pegs[pegNum.to_i-1].unshift thisDisc
@@ -26,6 +26,6 @@ line = infile.gets.split(' ')
 	end
 
 	# Seed the breadth-first layer
-	@this_layer.push start
+	@this_layer.push @start
 
 end	# Close the file being read
